72d425eadaf96b65b3261b0dadf1179cf68ff56c
Nicolas A. Gazaniga <nicolasgazaniga13@gmail.com>
2023-11-07 21:48:14 -0300

    07/11 - Continuação

--- my-app/src/AddList/AddList.js
+++ my-app/src/AddList/AddList.js
@@ -1,25 +1,104 @@
-import { View, Button, StyleSheet, TextInput, TouchableOpacity, Text } from "react-native";
+import { View, Button, StyleSheet, TextInput, TouchableOpacity, Text, Keyboard, Alert} from "react-native";
+import { FlatList } from "react-native";
+import { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 
 export function AddList({navigation}) {
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  async function addTask() {
+
+    if (newTask === '') {
+      return;
+    }
+
+    const search = task.filter(task => task === newTask);
+
+    if (search.length != 0) {
+      Alert.alert("Tarefa repetida!")
+      return;
+    }
+
+    setTask([ ...task, newTask]);
+    setNewTask('');
+    Keyboard.dismiss();
+  }
+
+  async function removeTask(item) {
+    Alert.alert(
+      "Deletar item",
+      "Tem certeza que deseja remover essa tarefa?",
+      [
+        {
+          text: "Cancelar",
+          onPress: () => {
+            return;
+          },
+          style: 'cancel'
+          },
+            {
+              text: "OK", onPress: () => setTask(task.filter(tasks => tasks != item))
+            }
+      ],
+      { cancelable: false }
+    )
+  } 
+
+  useEffect(() => {
+    async function loadData() {
+      const task = await AsyncStorage.getItem("task");
+
+      if (task) {
+        setTask(JSON.parse(task));
+      }
+    }
+    loadData();
+  }, []);
+
+  useEffect(() => {
+    async function saveData() {
+      AsyncStorage.setItem("task", JSON.stringify(task))
+    }
+    saveData();
+  }, [task]);
+
     return (
-        <View style={styles.container}>
-             <Button
-                title="AddList"
-                onPress={() => navigation.navigate('HomeScreen')}
-            />
-            <TextInput
-        style={styles.inputField}
-        placeholder="Adicione uma nova tarefa"
-        placeholderTextColor="#808080"
-      />
-      <TouchableOpacity 
-        style={styles.button
-        }>
-        <View style={styles.iconButton}>
-          <Text style={styles.textButton}>+</Text>
-        </View>
-      </TouchableOpacity>
-    </View>
+        <>
+          <View style={styles.container}>
+            <View style={styles.Body}>
+              <FlatList 
+              style={styles.FlatList}
+              data={task}
+              keyExtractor={item => item.toString()}
+              showsVerticalScrollIndicator={false}
+              renderItem={({ item }) => (
+                <View style={styles.ContainerView}>
+                  <Text style={styles.Text}>{item}</Text>
+                  <TouchableOpacity onPress={() => removeTask(item)}>
+                    <Text>Remove</Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+              />
+            </View>
+            <View style={styles.Form}>
+              <TextInput 
+              style={styles.Input} 
+              placeholderTextColor="#999" 
+              autoCorrect={true} 
+              placeholder="Adicione uma tarefa"
+              maxLength={25}
+              onChangeText={text => setNewTask(text)}
+              value={newTask}
+              />
+              <TouchableOpacity style={styles.Button} onPress={() => addTask()}>
+                <Text>Add</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </>
   );
 }
 
@@ -28,43 +107,71 @@ export default AddList;
 const styles = StyleSheet.create ({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#FFF",
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    marginTop:  20
+  },
+
+  Body: {
+    flex: 1
   },
   
-  inputField: {
+  Form: {
+    padding: 0,
+    height: 50,
+    justifyContent: "center",
+    alignSelf: "stretch",
+    flexDirection: "row",
+    paddingTop: 13,
+    borderTopWidth: 1, 
+    borderColor: "#EEE"
+  },
+
+  Input: {
     flex: 1,
-    height: 54,
-    padding: 16,
-    backgroundColor: '#262626',
-    borderRadius: 6,
-    marginRight: 4,
-    borderStyle: 'solid',
+    height: 40,
+    backgroundColor: "#eee", 
+    borderRadius: 4,
+    paddingVertical: 5, 
+    paddingHorizontal: 10,
     borderWidth: 1,
-    borderColor: '#0D0D0D',
-    color: '#F2F2F2',
-    fontSize: 16,
+    borderColor: "#eee"
   },
-  button: {
-    width: 52,
-    borderRadius: 6,
-    backgroundColor: '#1E6F9F',
-    justifyContent: 'center',
-    alignItems: 'center',
+
+  Button: {
+    height: 40,
+    width: 40,
+    justifyContent: "center", 
+    alignItems: "center",
+    backgroundColor: "#1c6cce",
+    borderRadius: 4,
+    marginLeft: 10
   },
-  iconButton: {
-    width: 18,
-    height: 18,
-    borderColor: '#F2F2F2',
-    borderStyle: 'solid',
-    borderWidth: 1.5,
-    borderRadius: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignContent: 'center'
-  },
-  textButton: {
-    lineHeight: 16,
-    color: '#F2F2F2',
+
+  FlatList: {
+    flex: 1,
+    marginTop: 5
+  }, 
+
+  ContainerView: {
+    marginBottom: 15,
+    padding: 15,
+    borderRadius: 4,
+    backgroundColor: "#eee",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    borderWidth: 1,
+    borderColor: "#eee",
+    alignItems: "center"
+  }, 
+
+  Text: {
+    fontSize: 14,
+    color: "#333",
+    fontWeight: "bold",
+    marginTop: 4,
+    textAlign: "center"
   }
-})
+});
