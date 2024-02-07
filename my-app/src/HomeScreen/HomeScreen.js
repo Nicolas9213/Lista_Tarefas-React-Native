d52010e07d40307eb18060000dea1f95fa4f550b
Nicolas A. Gazaniga <nicolasgazaniga13@gmail.com>
2024-02-07 19:04:55 -0300

    commit

--- my-app/src/HomeScreen/HomeScreen.js
+++ my-app/src/HomeScreen/HomeScreen.js
@@ -27,6 +27,7 @@ const styles = StyleSheet.create ({
     justifyContent: 'space-around'
   },
   texts: {
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
